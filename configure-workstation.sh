b1d6d2e78d888508a44eb388286c6762b03f7adc
Nikoquattro <nikoquattro@gmail.com>
2016-12-06 22:00:36 +0100

    modif affichage verification des droits 'root'

--- configure-workstation.sh
+++ configure-workstation.sh
@@ -13,6 +13,8 @@
 # Définiton des variables
 # -----------------------
 rouge='\e[1;31m'
+bleu='\e[1;34m'
+neutre='\e[0;m'
 
 # Vérification que le script est bien executé en tant que 'root'
 # --------------------------------------------------------------
@@ -20,14 +22,24 @@ if [[ $EUID -ne 0 ]]; then
     echo "******************************************************************************"
     echo "***                                                                        ***"
     echo "***                                                                        ***"
-    echo "***            ${rouge}This script must be run as root${neutre}            ***" 1>&2
+    echo -e "***                 ${rouge}!!! This script must be run as root !!!${neutre}                ***"
     echo "***                                                                        ***"
     echo "***                                                                        ***"
+    echo -e "*** ${rouge}You must run the script with the command 'sudo'${neutre}                        ***"
+    echo -e "*** ${rouge}Make sure you have read and understood this script before executing it${neutre} ***"
     echo "******************************************************************************"
+    1>&2
 	exit 1
 fi
 
 # Mise à jour de la distribution utilisée
 # ---------------------------------------
-echo "Update & upgrade"
-apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+echo "                                                                              "
+echo "                                                                              "
+echo "                                                                              "
+echo "******************************************************************************"
+echo "***                                                                        ***"
+echo -e "*** ${bleu}Update and upgrade software sources${neutre}                                    ***"
+echo "***                                                                        ***"
+echo "******************************************************************************"
+#apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
