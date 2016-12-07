452597cf7da9ae7a4ce2699ffbb62c8c2ecb8a8e
Nikoquattro <nikoquattro@gmail.com>
2016-12-07 19:09:06 +0100

    ajout de la creation d'un fichier de log + nettoyage pares installation et MAJ

--- configure-workstation.sh
+++ configure-workstation.sh
@@ -16,9 +16,13 @@ rouge='\e[1;31m'
 bleu='\e[1;34m'
 neutre='\e[0;m'
 
+
+NOW=$(date +"%m-%d-%Y")
+
 # Vérification que le script est bien executé en tant que 'root'
 # --------------------------------------------------------------
 if [[ $EUID -ne 0 ]]; then
+    clear
     echo "******************************************************************************"
     echo "***                                                                        ***"
     echo "***                                                                        ***"
@@ -32,14 +36,32 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+# Création d'un fichier de log
+# ----------------------------
+logFile="/tmp/configure-workstation-$NOW"
+touch $logFile
+redirect="2>&1 >> $logFile"
+
 # Mise à jour de la distribution utilisée
 # ---------------------------------------
-echo "                                                                              "
-echo "                                                                              "
-echo "                                                                              "
+clear
 echo "******************************************************************************"
 echo "***                                                                        ***"
 echo -e "*** ${bleu}Update and upgrade software sources${neutre}                                    ***"
 echo "***                                                                        ***"
 echo "******************************************************************************"
 #apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+
+# Nettoyage de l'installation
+# ---------------------------
+apt-get clean
+apt-get -q --yes autoremove --purge
+
+# prompt for a reboot
+# -------------------
+clear
+echo ""
+echo "===================="
+echo " TIME FOR A REBOOT! "
+echo "===================="
+echo ""
